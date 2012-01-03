0b4aaf6ffd7a39158c4a67ac3209d11205a2ef63
César Salazar <i.am@cesarsalazar.mx>
2012-01-03 15:45:31 -0600

    Adding README

--- app.rb
+++ app.rb
@@ -9,12 +9,12 @@ require 'rdiscount'
 require 'nokogiri'
 
 get '/' do
-  @index = RDiscount.new( File.open("content/index.md").read ).to_html
+  @index = RDiscount.new( File.open("contents/index.md").read ).to_html
   haml :index
 end
 
 get '/:article' do
-  @content = RDiscount.new( File.open("content/" + params["article"].gsub("-", "_").concat(".md")).read ).to_html
+  @content = RDiscount.new( File.open("contents/" + params["article"].gsub("-", "_").concat(".md")).read ).to_html
   doc_title = Nokogiri::HTML::DocumentFragment.parse( @content ).css('h1').inner_html()  
   @title = "#{doc_title} | Observaciones de un explorador, por César Salazar"
   haml :article
